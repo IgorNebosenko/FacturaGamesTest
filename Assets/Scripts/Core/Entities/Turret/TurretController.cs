9dcdf0c0d77191caaf3583218f3ced15d01d647c
IhorNebosenko <igor.nebosenko@gmail.com>
2025-07-17 14:15:43 +0300

    Add turret controller

---
+++ Assets/Scripts/Core/Entities/Turret/TurretController.cs
@@ -0,0 +1,25 @@
+﻿using Core.Input;
+using UnityEngine;
+using Zenject;
+
+namespace Core.Entities.Turret
+{
+    public class TurretController : MonoBehaviour
+    {
+        private PlayerInput _playerInput;
+        
+        [Inject]
+        private void Construct(InputActions inputActions)
+        {
+            _playerInput = new PlayerInput(inputActions);
+            _playerInput.Init();
+        }
+
+        private void FixedUpdate()
+        {
+            _playerInput.Update();
+            
+            //Todo translate to rotation position
+        }
+    }
+}
